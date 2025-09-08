373e144aec97559c6be881be4151a315242b9413
Esai Barron <esaibarron@gmail.com>
2025-09-07 20:20:00 -0500

    implemented list of vehilces in dashboard

--- app/routes.py
+++ app/routes.py
@@ -1,20 +1,32 @@
 from flask import redirect, render_template, Blueprint, url_for
 from app.models import Vehicle
 from flask_login import login_required, current_user, logout_user
+from app import objDB
+from sqlalchemy import text
 
 objBP = Blueprint('main', __name__)
 
 @objBP.route('/')
 def index():
-    vehicles = Vehicle.query.all()
-    return render_template('index.html', vehicles=vehicles)
+    
+    return render_template('index.html')
 
 
 @objBP.route('/dashboard')
 @login_required
 def dashboard():
+
+    vhcSql = "SELECT * FROM Vehicle WHERE assigned_to = :user_id"
+
+    print ("bro")
+
+    vehicles = objDB.session.execute(text(vhcSql), {"user_id": current_user.id}).fetchall()
     # Example protected page
-    return f"Welcome {current_user.username}! You are logged in as {current_user.role}."
+
+    print ("bro1")
+   
+    return render_template('dashboard.html',username = current_user.username, role = current_user.role, vehicles=vehicles)
+    
 
 @objBP.route('/logout')
 @login_required
